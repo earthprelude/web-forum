b8494f8ae2745d7daa48a8da41f4980d45543195
EP <earthprelude8@gmail.com>
2021-09-26 21:47:08 -0700

    updated settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = 'b#s*_o(3t3ai_k(c5po@h7a=nj5#vjkd3u7ckhnx@)mi=8fn67'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+#ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = []
 X_FRAME_OPTIONS = 'ALLOW ALL'
 
 # Application definition
